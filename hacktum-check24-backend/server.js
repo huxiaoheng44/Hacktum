c7682943d0319307b79396afa6abd8b3204ef5df
yoying-wu <114493294+yoying-wu@users.noreply.github.com>
2023-11-18 13:06:35 +0100

    mock backend

--- hacktum-check24-backend/server.js
+++ hacktum-check24-backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
+const app = express();
 // const mongoose = require("mongoose");
 const cors = require("cors");
-
 const api = require("./api.js");
 
 // Need to add Server IP address in MongoDB
@@ -20,12 +20,16 @@ const options = {
 //   .then(() => console.log("Connected to MongoDB"))
 //   .catch((err) => console.log(`Error connecting to MongoDB: ${err}`));
 
-// Create a express server
-const app = express();
-
 app.use(express.json());
 app.use(cors());
 app.use("/api", api);
+
+// const dataSchema = new mongoose.Schema({
+//   id: Number,
+//   name: String,
+//   postalCode: String,
+//   // rankingScore: String,
+// });
 
 // server error handle
 app.use((err, req, res, next) => {
@@ -43,7 +47,47 @@ app.use((err, req, res, next) => {
   });
 });
 
+app.get("/craftsmen", async (req, res) => {
+  // ideally should be validated & sanitzed.
+  const { postalcode } = req.query;
+
+  // const craftsmen = await getCraftsmen(postalcode);
+
+  // const rankedCraftsmen = craftsmen.map((item) => {
+  //   return { ...item.toObject(), rankingScore: "calculateRanking" };
+  // });
+
+  const rankedCraftsmen = {
+    craftsmen: [
+      {
+        id: 1,
+        name: "Leon Ramos",
+        postalCode: "80686",
+        rankingScore: "5.46",
+      },
+      {
+        id: 2,
+        name: "Constant Woolery",
+        postalCode: "80687",
+        rankingScore: "3.27",
+      },
+      {
+        id: 3,
+        name: "Eva Malone",
+        postalCode: "80689",
+        rankingScore: "4.43",
+      },
+    ],
+  };
+
+  return res.send({
+    craftsmen: rankedCraftsmen, //craftsmen.map(getMappedCraftsman),
+  });
+});
+
 // start the server
-app.listen(3000, () => {
+const port = process.env["SERVER_PORT"] || 3000;
+
+app.listen(port, () => {
   console.log("Server is running on port 3000");
 });
