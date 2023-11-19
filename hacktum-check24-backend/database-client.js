e9b41b1dce6321973c9b1f954dd6f6aff78c817f
yoying-wu <114493294+yoying-wu@users.noreply.github.com>
2023-11-19 01:28:46 +0100

    update docker compose file

--- hacktum-check24-backend/database-client.js
+++ hacktum-check24-backend/database-client.js
@@ -8,7 +8,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: process.env["DB_USER"],
-  host: process.env["DB_HOST"],
+  host: "postgres",
   database: process.env["DB_NAME"],
   password: process.env["DB_PASSWORD"],
   port: process.env["DB_PORT"], // Default PostgreSQL port
